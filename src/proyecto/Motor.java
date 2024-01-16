6b43472a11c7d69d650bb5b4770e3df5626ffcaf
mayrabpi <145108717+mayrabpi@users.noreply.github.com>
2024-01-16 21:54:07 +0100

    creando algunos métodos

--- src/proyecto/Motor.java
+++ src/proyecto/Motor.java
@@ -6,24 +6,87 @@ import java.util.Scanner;
  */
 
 public class Motor {
-	public enum tColores{Rojo,Verde,Azul,Dorado};
+	public enum tColores{ROJO,VERDE,AZUL,DORADO};
+	
+	
+    	
 	private Scanner entrada=new Scanner(System.in);
-    private  int max_colores_seq;
+    final private  int max_colores_seq=12;
     private tColores [] secuenciaColores = new tColores[max_colores_seq];
     private String nombre;
-    
+  /**
+   * constructor  
+   */
     
     public Motor(){
-    	this.max_colores_seq = 12;
-    	this.nombre = entrada.nextLine();
-    	enum tColores{Rojo,Verde,Azul,Dorado};
-    	tColores [] secuenciaColores = new tColores[max_colores_seq];
-    	char _color=  entrada.next().charAt(' ');
     	
+    	this.nombre = entrada.nextLine();
+    	tColores [] secuenciaColores = new tColores[max_colores_seq];
+    	char _color = entrada.next().charAt(' ');
+    	int _numero;
     	
     	
     }
-
+    /**
+     * metodo charToColor, este método recibe un char y devuelve un color del tipo enum tColeres
+     * @param _color
+     * @return
+     */
+  public tColores charToColor (char _color) {
+	  tColores charColor=null ;
+	  switch (_color) {
+	  case 'R':
+		  charColor= tColores.ROJO;
+		  break;
+	  case 'V':
+		  charColor= tColores.VERDE;
+		  break;
+	  case 'A':
+		  charColor= tColores.AZUL;
+		  break;
+	  case 'D':
+		  charColor= tColores.DORADO;
+		  break;
+	  	  
+	  }
+		return charColor;
+		
+		/**
+		 * Método intColor, este método recibe un int y devuelve la posición del color
+		 */
+  }
+    
+    public tColores intTocolor (int _numero) {
+    	 tColores posicion=null;
+    	switch (_numero) {
+    	case 0:
+    		posicion= tColores.ROJO;
+    		break;
+    	case 1:
+    		posicion= tColores.VERDE;
+    		break;
+    	case 2: 
+    		posicion= tColores.AZUL;
+    		break;
+    	case 3:
+    		posicion=tColores.DORADO;
+    		break;
+    		
+    	}
+    	return posicion;
+    	
+    	/**
+    	 * Método generarSecuencia 
+    	 */
+    }
+    public void generarSecuencia (int _numColores) {
+    	
+    }
+   public void menu() {
+	   System.out.println("¿Que deseas hacer? \\n: 0 Salir \\n 1 Jugar ");
+   }
+    
+    
 	
 	
 			
