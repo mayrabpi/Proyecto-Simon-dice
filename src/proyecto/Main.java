ac688843394bec8fadc2f2d849387af296464546
mayrabpi <145108717+mayrabpi@users.noreply.github.com>
2024-01-18 21:49:25 +0100

    mas metodos

--- src/proyecto/Main.java
+++ src/proyecto/Main.java
@@ -4,7 +4,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Motor miJuego = new Motor();
+		
+	miJuego.play();
+	
+	
+		
 
+       
 	}
+	
+	
 
 }
