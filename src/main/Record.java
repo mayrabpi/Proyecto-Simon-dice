65b0e2aa82d46ab7c8ef7493528c6976a3d10fa3
mayrabpi <145108717+mayrabpi@users.noreply.github.com>
2024-02-13 18:40:43 +0100

    creando_metodos_en Record

--- src/main/Record.java
+++ src/main/Record.java
@@ -1,5 +1,50 @@
 package main;
-
+/**
+ * clase Record
+ * 
+ */
 public class Record {
+	private final int MAX_JUGADORES=10;
+	private int contador;
+	private Jugador []jugadores = new Jugador[MAX_JUGADORES];
+	
+	/**
+	 * constructor
+	 */
+	public Record() {
+		this.contador=0;		
+	}
+	/**
+	 * 
+	 * @return contador
+	 */
+	public int getContador() {
+		return this.contador;
+	}
+	public String getjugadorNombre(String nombre) {
+		//while
+		return nombre;
+	}
+	/**
+	 * Ordena el Array, usaremos un algoritmo del metodo de la burbuja 
+	 */
+	public void ordenarRanking() {
+		
+	}
+	/**
+	 * Muestra el ranking de los 10 mejores jugadores
+	 */
+	public void showRanking() {
+		
+	}
+	/**
+	 * Muestra el jugador (o jugadores) con la puntuacion mas alta 
+	 */
+	public void showBestPlayer() {
+		
+	}
+	
+	
+	
 
 }
